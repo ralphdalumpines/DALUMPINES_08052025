2efc0e1f1ae368601b21ac06ff0d8e79c7bf7b59
ralphdalumpines <ralphjosephdalumpines@gmail.com>
2025-08-06 02:57:11 +0800

    Fix for missing properties

--- VideoServiceApp/VideoService.FE/App-FE/src/app/pages/list-videos/list-videos.component.ts
+++ VideoServiceApp/VideoService.FE/App-FE/src/app/pages/list-videos/list-videos.component.ts
@@ -16,28 +16,32 @@ export class ListVideosComponent {
       thumbnail: 'assets/video1-thumbnail.jpg',
       title: 'Sample Video 1',
       description: 'This is a sample video description for video 1.',
-      categories: ['Category1', 'Category2']
+      categories: ['Category1', 'Category2'],
+      videoUrl: 'assets/video1.mp4'
     },
     {
-        id: 2,
+      id: 2,
       thumbnail: 'assets/video2-thumbnail.jpg', 
       title: 'Sample Video 2',
       description: 'This is a sample video description for video 2.',
-      categories: ['Category2', 'Category3']
+      categories: ['Category2', 'Category3'],
+      videoUrl: 'assets/video2.mp4'
     },
     {
-        id: 3,
+      id: 3,
       thumbnail: 'assets/video3-thumbnail.jpg',
       title: 'Sample Video 3',
       description: 'This is a sample video description for video 3.',
-      categories: ['Category1', 'Category3']
+      categories: ['Category1', 'Category3'],
+      videoUrl: 'assets/video3.mp4'
     },
     { 
-        id: 4,
+      id: 4,
       thumbnail: 'assets/video4-thumbnail.jpg',
       title: 'Sample Video 4',
       description: 'This is a sample video description for video 4.',
-      categories: ['Category1', 'Category2', 'Category3']
+      categories: ['Category1', 'Category2', 'Category3'],
+      videoUrl: 'assets/video4.mp4'
     } 
   ]);
 }
